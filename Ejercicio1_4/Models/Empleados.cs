83070589b66f31debf938e83a48c67b72777fbf5
joseluis29 <joseluisr765401@gmail.com>
2023-02-27 22:31:53 -0600

    Add project files.

---
+++ Ejercicio1_4/Models/Empleados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using SQLite;
+using System.Collections.Generic;
+
+
+namespace Ejercicio1_4.Models
+{
+  public  class Empleados
+    {
+        [PrimaryKey, AutoIncrement]
+        public int codigo { get; set; }
+
+        public String nombres { get; set; }
+
+        public String descripcion { get; set; }
+
+        public byte[] imagen { get; set; }
+    }
+}
